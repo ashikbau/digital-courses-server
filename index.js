6b02f5d2059c31de542f041fba900b30a34deb37
Your Name <you@example.com>
2022-10-25 22:47:14 +0200

    added dynamic api

--- index.js
+++ index.js
@@ -6,16 +6,43 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+// const coursesDetails = require('./data/courseDetail.json');
+
+
+
+
+
+
+
 
 app.get('/course-categories', (req, res) => {
     res.send(courses)
 });
+app.get('/courses', (req, res) => {
+    res.send(courses)
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    
+    const course=courses.find(c=>c.id===parseInt(id))
+    if(!course) res.status(400).send('this ID is not found')
+    else{
+        res.send(course)
+    }
+    
+    
+    
+});
+
 
 
 
 app.get('/', (req, res) => {
     res.send('News API Running');
 });
+
+
 
 
 app.listen(port, () => {
